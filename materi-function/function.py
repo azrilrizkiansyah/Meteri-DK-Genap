417c5c96ce2ed5c17165ab88e4d725aba68c3576
fahrizalrakhagunawan <agoentea30@gmail.com>
2025-02-11 07:43:54 +0700

    manambahkan materi function

---
+++ materi-function/function.py
@@ -0,0 +1,10 @@
+def CetakIndentitasSiswa(nama,kelas,jenis_kelamin):
+    print("nama : ",nama)
+    print("kelas : ",kelas)
+    print ("jenis kelamin : ",jenis_kelamin)
+    
+#panggil fungsi cetak indentitas siswa
+CetakIndentitasSiswa("cipung ", "X - PLL", "laki-laki")
+CetakIndentitasSiswa("el ", "X - PLL", "laki-laki")
+CetakIndentitasSiswa("galdi ", "X - PLL", "laki-laki")
+CetakIndentitasSiswa("Rudius ", "X - PLL", "laki-laki")
